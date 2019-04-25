408ca3f684ca329fefbf15bd7c858772df2c8956
jfr_c <jfrcro@live.com>
2019-04-25 17:59:54 -0500

    funciona ciclo requerimiento treintalineas

--- dibujo.js
+++ dibujo.js
@@ -1,11 +1,15 @@
 var d = document.getElementById("dibujito");
 var lienzo = d.getContext("2d");
+
 var lineas = 30;
 var l = 0;
 
+
 while(l < lineas)
 {
-  dibujarLinea("#AAF", 10, 200, 280, 10);
+  var yinicial = 10 * l; 
+  var xfinal = 10 * (l + 1);
+  dibujarLinea("#AAF", 0, yinicial, xfinal, 300);
   console.log("linea " + l)
   l = l + 1;
 }
