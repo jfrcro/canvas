189bb52f7b7121cc113166351df2dc8e1dbf76a5
jfr_c <jfrcro@live.com>
2019-04-25 17:38:19 -0500

    estructura sintaxis funcion aplicado al canvas

--- dibujo.js
+++ dibujo.js
@@ -2,13 +2,15 @@ var d = document.getElementById("dibujito");
 var lienzo = d.getContext("2d");
 console.log(lienzo);
 
+dibujarLinea("pink", xinicial, yinicial, xfinal, yfinal);
 
-dibujarLinea(color, xinicial, yinicial, xfinal, yfinal);
-
-lienzo.beginPath();
-lienzo.strokeStyle = color;
-lienzo.moveTo(xinicial, yinicial);
-lienzo.lineTo(xfinal, yfinal);
-lienzo.stroke();
-lienzo.colsePath();
+Function dibujarLinea(color, xinicial, yinicial, xfinal, yfinal);
+{
+	lienzo.beginPath();
+	lienzo.strokeStyle = color;
+	lienzo.moveTo(xinicial, yinicial);
+	lienzo.lineTo(xfinal, yfinal);
+	lienzo.stroke();
+	lienzo.colsePath();
+}
 
