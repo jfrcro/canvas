e47ac2fd48affe5b0e460ddf865202817eb5cc5b
jfr_c <jfrcro@live.com>
2019-04-25 17:41:55 -0500

    cuclo para controlar la invocacion + depuracion + otras pruebas

--- dibujo.js
+++ dibujo.js
@@ -1,15 +1,23 @@
 var d = document.getElementById("dibujito");
 var lienzo = d.getContext("2d");
+var lineas = 30;
+var l = 0;
 
-dibujarLinea("pink", 10, 300, 220, 10);
+while(l < lineas)
+{
+  dibujarLinea("#AAF", 10, 200, 280, 10);
+  console.log("linea " + l)
+  l = l + 1;
+}
 
 function dibujarLinea(color, xinicial, yinicial, xfinal, yfinal)
 {
 	lienzo.beginPath();
 	lienzo.strokeStyle = color;
+  // lienzo.strokeWidht = "3px";
 	lienzo.moveTo(xinicial, yinicial);
 	lienzo.lineTo(xfinal, yfinal);
 	lienzo.stroke();
-	lienzo.colsePath();
+	lienzo.closePath();
 }
 
