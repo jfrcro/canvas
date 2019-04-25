9365441519931b6ade1a526bc4ac465327c16386
jfr_c <jfrcro@live.com>
2019-04-25 17:39:52 -0500

    estructura sintaxis funcion aplicado al canvas con parametros, primer linea de canvas

--- dibujo.js
+++ dibujo.js
@@ -1,10 +1,9 @@
 var d = document.getElementById("dibujito");
 var lienzo = d.getContext("2d");
-console.log(lienzo);
 
-dibujarLinea("pink", xinicial, yinicial, xfinal, yfinal);
+dibujarLinea("pink", 10, 300, 220, 10);
 
-Function dibujarLinea(color, xinicial, yinicial, xfinal, yfinal);
+function dibujarLinea(color, xinicial, yinicial, xfinal, yfinal)
 {
 	lienzo.beginPath();
 	lienzo.strokeStyle = color;
