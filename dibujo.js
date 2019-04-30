f702f874f852b4eb9c7decb861f93ec8d8dded54
jfr_c <jfrcro@live.com>
2019-04-30 17:48:59 -0500

    boton y texto con atributos + inclui escuchador en js

--- dibujo.js
+++ dibujo.js
@@ -1,21 +1,30 @@
+var	texto = document.getElementById("texto_lineas");
+var	boton = document.getElementById("botoncito");
+
+boton.addEventListener("click", dibujoPorClick);
+
+function dibujoPorClick()
+{
+	alert("escuchando");
+}
+
 var d = document.getElementById("dibujito");
 var lienzo = d.getContext("2d");
 
 var lineas = 30;
 var l = 0;
 
+var yinicial, xfinal;
 var colorLineas = "#AAF";
 
-dibujarLinea(colorLineas, 0, 0, 300, 0);
-dibujarLinea(colorLineas, 300, 0, 300, 300);
-
+dibujarLinea(colorLineas, 0, 0, 0, 300);
+dibujarLinea(colorLineas, 0, 300, 300, 300);
 
 for(l = 0; l < lineas; l++)
 {
-  baseCero = 10 * l; 
-  baseDiez = 10 * (l + 1);
-  dibujarLinea(colorLineas, baseCero, 0, 300, baseDiez);
-  dibujarLinea(colorLineas, 0, baseCero, baseDiez, 300);
+  yinicial = 10 * l; 
+  xfinal = 10 * (l + 1);
+  dibujarLinea(colorLineas, 0, yinicial, xfinal, 300);
   console.log("linea " + l)
 }
 
@@ -29,4 +38,3 @@ function dibujarLinea(color, xinicial, yinicial, xfinal, yfinal)
 	lienzo.stroke();
 	lienzo.closePath();
 }
-
